0eb86d3b8f68cc60f5eac7e675201b7040ccb36a
Luis Guilherme <luislebert01@outlook.com>
2023-04-03 15:26:41 -0300

    Mudanças na classe cliente

--- prjHotelaria/Entities/Client.cs
+++ prjHotelaria/Entities/Client.cs
@@ -3,20 +3,24 @@
     internal class Client : Reserve
     {
         public string Name { get; set; }
+        public string CPF { get; set; }
         public DateTime BirthDate { get; set; }
         public bool Status { get; set; }
 
-        public Client(string name, DateTime birth, string cpf, int room, DateTime checkin, DateTime checkout)
+        public Client(string name, DateTime birth, string cpf)
         {
             Name = name;
+            CPF = cpf;
             BirthDate = birth;
             Status = true;
         }
 
-        public void ChangeStatus()
+        public void ChangeStatus(bool status) { Status = status; }
+
+        public bool LegalAge()
         {
-            if (Status) Status = false;
-            else Status = true;
+            TimeSpan timeSpan = (DateTime.Now.Date - BirthDate.Date);
+            return (timeSpan.TotalDays / 365.25) > 18;
         }
     }
 }
