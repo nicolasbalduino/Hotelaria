4646844a13f7c667f101a40aa26a8d4c943b8b16
Luis Guilherme <luislebert01@outlook.com>
2023-04-02 22:54:43 -0300

    Entidade reserva adicionada

---
+++ prjHotelaria/Entities/Reserve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjHotelaria.Entities
+{
+    internal class Reserve
+    {
+        public string CPF { get; private set; }
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public int RoomNumber { get; set; }
+        public double TotalDue { get; private set; }
+
+        public void SetCheckIn(DateTime checkin) { CheckinDate = checkin; }
+        public void SetCheckOut(DateTime checkout) { CheckoutDate = checkout; }
+
+    }
+}
