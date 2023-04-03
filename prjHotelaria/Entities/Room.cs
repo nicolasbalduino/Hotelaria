29d0d4df33d29603499b964da21ece4f9ff7fa5c
Luis Guilherme <luislebert01@outlook.com>
2023-04-03 15:30:39 -0300

    Criada entidade quarto

---
+++ prjHotelaria/Entities/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjHotelaria.Entities
+{
+    internal class Room
+    {
+        public int RoomNumber { get; private set; }
+        public Room(int room) { RoomNumber = room; }
+        public double PerNightValue => RoomNumber * 100;
+    }
+}
