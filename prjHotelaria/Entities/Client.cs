2999b80afcacdfcce1d0ebda9d522a6780d89e54
Luis Guilherme <luislebert01@outlook.com>
2023-04-04 08:49:55 -0300

    Remoção da propriedade Status de cliente

--- prjHotelaria/Entities/Client.cs
+++ prjHotelaria/Entities/Client.cs
@@ -5,17 +5,13 @@
         public string Name { get; set; }
         public string CPF { get; set; }
         public DateTime BirthDate { get; set; }
-        public bool Status { get; set; }
 
         public Client(string name, DateTime birth, string cpf)
         {
             Name = name;
             CPF = cpf;
             BirthDate = birth;
-            Status = true;
         }
-
-        public void ChangeStatus(bool status) { Status = status; }
 
         public bool LegalAge()
         {
