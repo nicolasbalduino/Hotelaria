ea6a48f8c119c3b3e23c69630f2638e5fff5ce27
Luis Guilherme <luislebert01@outlook.com>
2023-04-04 08:49:24 -0300

    Mudança no armazenamento de acompanhante na reserva, mudança no cálculo para total da reserva

--- prjHotelaria/Entities/Reserve.cs
+++ prjHotelaria/Entities/Reserve.cs
@@ -3,48 +3,40 @@
     internal class Reserve
     {
         public string ContractorCPF { get; private set; }
-        public string? CompanionCPF { get; private set; }
+        public Client Companion { get; private set; }
         public DateTime CheckinDate { get; set; }
         public DateTime CheckoutDate { get; set; }
         public int Days { get; set; }
         public int RoomNumber { get; set; }
-        public double CompTotal { get; private set; }
-        public bool CheckIn { get; private set; }
-        public bool Breakfast { get; private set; }
+        public double Total { get; set; }
+        public int Status { get; set; }
 
         public Reserve(string contractor, DateTime checkin, DateTime checkout, int room, double total, bool isCheckin, bool breakfast)
         {
             ContractorCPF = contractor;
             CheckinDate = checkin;
             CheckoutDate = checkout;
-            Breakfast = breakfast;
             Days = (int)(CheckoutDate.Date - CheckinDate.Date).TotalDays;
             RoomNumber = room;
-            CheckIn = isCheckin;
         }
 
-        public void CheckCompanion(string cpf) { CompanionCPF = cpf; }
+        public void CheckCompanion(Client companion) { Companion = companion; }
 
-        public double RoomTotal() => Math.Ceiling((int)RoomNumber/20) * 100 * Days;
-        public double BreakFastTotal() => (CompanionCPF == null) ? Days * 20 : (Days * 20) * 2;
-        public double CompanionTotal(DateTime companionDate)
+        public double RoomTotal(double room, bool breakFast)
         {
-            TimeSpan timeSpan = (DateTime.Now.Date - companionDate);
-            bool age = (timeSpan.TotalDays / 365.25) > 18;
+            Total = room * Days;
+            if (Companion.LegalAge()) Total += (room * 0.5 * Days);
+            if (breakFast) Total += (Days * 20);
+            if (Companion != null && breakFast) Total += (Days * 20);
 
-            if (!age) return 0;
-
-            CompTotal = (RoomTotal() * 0.50);
-            return CompTotal;
-
+            return Total;
         }
-
-        public double Total() => RoomTotal() + (Breakfast ? BreakFastTotal() : 0) + CompTotal;
 
         public void SetCheckIn(DateTime checkin) { CheckinDate = checkin; }
         public void SetCheckOut(DateTime checkout) { CheckoutDate = checkout; }
+        public void CalculateDays() { Days = (int)(CheckoutDate.Date - CheckinDate.Date).TotalDays; }
 
-        public void RealizeCheckin() { CheckIn = true; }
+        public void SetStatus(int opt) { Status = opt; }
     }
 
 }
